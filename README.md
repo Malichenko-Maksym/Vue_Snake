63d805401380b9d8f1322fe700ba085447e55046
Maksym Malichenko <s221680@student.uek.krakow.pl>
2024-02-08 01:06:03 +0100

    finished project

---
+++ README.md
@@ -0,0 +1,17 @@
+### Game mechanics
+- Playable with arrow keys left & right
+- Stores highscores in the local storage of a browser
+
+### To run:
+1.
+```
+npm install
+```
+2.
+```
+npm run serve
+```
+3. Open browser
+```
+http://localhost:<port>
+```
---
+++ src/components/GameLogo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="game-logo">
+    <picture>
+      <source srcset="@/assets/snake.webp" type="image/webp" />
+      <source srcset="@/assets/snake.png" type="image/png" />
+      <img
+        class="game-logo-img"
+        src="@/assets/snake.png"
+        alt="Snake Logo"
+      />
+    </picture>
+    <div class="game-logo-text">
+      <h1 class="game-logo-title">Snake</h1>
+      <h2 class="game-logo-subtitle">Smaczna kolacja!</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameLogo",
+};
+</script>
+
+<style>
+.game-logo {
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+}
+.game-logo-img {
+  display: block;
+  width: 6rem;
+}
+.game-logo-text {
+  line-height: 1.2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.game-logo-title {
+  font-size: 3rem;
+  font-weight: 800;
+  color: var(--col-prim);
+}
+.game-logo-subtitle {
+  font-size: 1.4rem;
+  font-weight: 600;
+  color: var(--col-txt-sec);
+}
+</style>
